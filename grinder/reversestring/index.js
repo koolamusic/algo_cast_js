06bada7c928ccacafac01072a6e266c7eec4a170
Andrew Miracle <me@andrewmiracle.com>
2019-02-01 17:35:28 +0100

    add debugger instructions

--- grinder/reversestring/index.js
+++ grinder/reversestring/index.js
@@ -6,6 +6,7 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// using the array reverse method
 function reverse(str) {
   return str
     .split("")
@@ -16,6 +17,26 @@ function reverse(str) {
 // using reduce accumulator and current value
 function reverse(str) {
   return str.split("").reduce((acc, curValue) => curValue + acc);
+}
+
+// using the for of syntax
+function reverse(str) {
+  // we initially define reversed as an empty string in within the function
+  let reversed = "";
+
+  // The for of loop iterates over elements
+  for (var character of str) {
+    // var reversed = []
+    // store the value into reversed recursively
+    reversed = character + reversed;
+
+    // use a debugger 
+    // - node inspect file.js
+    // This can iterate over for loops, reduce, map statements so you can see behind the hood
+    // For functions, call the functions manually
+  }
+  // return the value of reveresed in our function
+  return reversed;
 }
 
 module.exports = reverse;
